3ca4540833c966443fbee861d57cb900eeed435f
Fernando Rezk <fernandorezk@outlook.com>
2018-07-20 10:15:49 -0300

    Implementing the last steps of JUnit Tests

--- salestaxes-service/src/test/java/br/com/rezk/salestaxes/service/AppTest.java
+++ salestaxes-service/src/test/java/br/com/rezk/salestaxes/service/AppTest.java
@@ -4,25 +4,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import br.com.rezk.salestaxes.service.engine.CalculationEngine;
 import br.com.rezk.salestaxes.service.enums.NonTaxableProductEnum;
+import br.com.rezk.salestaxes.service.enums.TaxValue;
 import br.com.rezk.salestaxes.service.enums.TaxableProductEnum;
 import br.com.rezk.salestaxes.service.request.SalesTaxesRequest;
 import br.com.rezk.salestaxes.service.response.SalesTaxesResponse;
+import br.com.rezk.salestaxes.service.utils.Round;
 import junit.framework.TestCase;
 
-/**
- * Unit test for simple App.
- */
-@RunWith(SpringJUnit4ClassRunner.class)
 public class AppTest extends TestCase
 {
-	@Autowired
-	private CalculationEngine calculationEngine;
+												// Simulating lambda expression ( Java 8 )
+	private CalculationEngine calculationEngine = (List<SalesTaxesRequest> request) -> {
+		SalesTaxesResponse response = new SalesTaxesResponse();
+		Double salesTaxes = new Double(0);
+		Double total = new Double(0);
+		response.setProducts(request);
+		for(SalesTaxesRequest re : response.getProducts()) {
+			Double taxApplied = new Double(0);
+			Double originalPrice = re.getPrice();
+			if(NonTaxableProductEnum.get(re.getIdProductType()) == null) {
+				taxApplied += re.getPrice() * TaxValue.BASIC.getTax() * re.getQuantity();
+				re.setNameProductType(TaxableProductEnum.get(re.getIdProductType()).getName());
+				re.setIdProductType(null);
+			}
+			if(re.getImported()) {
+				taxApplied += re.getPrice() * TaxValue.IMPORTED.getTax() * re.getQuantity(); 
+			}
+			if(re.getIdProductType() != null) {
+				re.setNameProductType(NonTaxableProductEnum.get(re.getIdProductType()).getName());
+				re.setIdProductType(null);
+			}
+			re.setPrice(Round.roundUp(re.getPrice() * re.getQuantity() + taxApplied));
+			total += re.getPrice();
+			salesTaxes += Round.round(re.getPrice() - originalPrice);
+			
+		}
+		response.setSalesTaxes(Round.round(salesTaxes));
+		response.setTotal(Round.round(total));
+		return response;
+	};
 	
 	@Test
     public void testCase1() {
@@ -96,6 +119,40 @@ public class AppTest extends TestCase
 		SalesTaxesRequest product4 = new SalesTaxesRequest(1l, true, NonTaxableProductEnum.FOOD.getId(), 11.25);
 		request.add(product1);
 		request.add(product2);
+		request.add(product3);
+		request.add(product4);
 		SalesTaxesResponse response = calculationEngine.calculateSalesTaxes(request);
+		
+		SalesTaxesRequest product = response.getProducts().get(0);
+		{
+			assertEquals(product.getQuantity(), (Long) 1l);
+			assertEquals(product.getImported().booleanValue(), true);
+			assertEquals(product.getNameProductType(), "Bottle of Perfume");
+			assertEquals(product.getPrice(), 32.19);
+		}
+		
+		product = response.getProducts().get(1);
+		{
+			assertEquals(product.getQuantity(), (Long) 1l);
+			assertEquals(product.getImported().booleanValue(), false);
+			assertEquals(product.getNameProductType(), "Bottle of Perfume");
+			assertEquals(product.getPrice(), 20.89);
+		}
+		
+		product = response.getProducts().get(2);
+		{
+			assertEquals(product.getQuantity(), (Long) 1l);
+			assertEquals(product.getImported().booleanValue(), false);
+			assertEquals(product.getNameProductType(), "Medical");
+			assertEquals(product.getPrice(), 9.75);
+		}
+		
+		product = response.getProducts().get(3);
+		{
+			assertEquals(product.getQuantity(), (Long) 1l);
+			assertEquals(product.getImported().booleanValue(), true);
+			assertEquals(product.getNameProductType(), "Box of chocolates");
+			assertEquals(product.getPrice(), 11.85);
+		}
     }
 }
